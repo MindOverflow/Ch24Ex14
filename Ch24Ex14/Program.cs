676034df9d4fa9304871b1d5efd82b288c8101d3
MindOverflow <toxaSmith@yahoo.com>
2017-03-28 22:38:15 +0300

    [add] Initial commit.

---
+++ Ch24Ex14/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+using System.Threading.Tasks;
+
+namespace Ch24Ex14
+{
+    internal static class Program
+    {
+
+        private static int[] _data;
+
+        private static void DisplayData(int v, ParallelLoopState pls)
+        {
+            if (v < 0)
+            {
+                pls.Break();
+            }
+            WriteLine($"Значение: {v}");
+        }
+
+
+        private static void Main()
+        {
+            WriteLine("Основной поток выполнения запущен.");
+
+            _data = new int[100000000];
+
+            // Последовательный вариант инициализации массива в цикле.
+            for (var i = 0; i < _data.Length; i++)
+            {
+                _data[i] = i;
+            }
+
+            _data[100] = -10;
+
+            var parallelLoopResult = Parallel.ForEach(_data, DisplayData);
+
+            if (!parallelLoopResult.IsCompleted)
+            {
+                WriteLine(
+                    "\nЦикл завершился преждевременно из-за того, " + 
+                    "что обнаружено отрицательное значение\n" +
+                    $"на шаге цикла номер: {parallelLoopResult.LowestBreakIteration}. \n");
+            }
+
+            WriteLine("Основной поток выполнения завершён.");
+        }
+    }
+}
